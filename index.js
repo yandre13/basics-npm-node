197f6f9edbd92a7286a069e2c925f253a7f79005
yandre13 <yandre13@hotmail.com>
2020-05-14 09:52:54 -0500

    adding index.js and package.json

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	hello: function () {
+		console.log('hello')
+	},
+	customHello: function (name) {
+		console.log('hello ', name)
+	},
+	helloNpm: function () {
+		console.log('hello npm')
+	},
+}
